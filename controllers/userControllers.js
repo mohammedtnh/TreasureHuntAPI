3255f8050f9b913bb3c7e75f6a169e810a59c2e3
Mohamed Hamdan <mohamedhamdan@Mohameds-MacBook-Pro.local>
2021-02-23 12:21:26 +0300

    sending email added

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,6 +2,38 @@ const bcrypt = require("bcrypt");
 const { User } = require("../db/models");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET, JWT_EXPIRATION_MS } = require("../config/keys");
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (newUser) => {
+  try {
+    let testAccount = await nodemailer.createTestAccount();
+
+    let transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass, // generated ethereal password
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: `"Treasure Hunt Team 👻" ${testAccount.user}`, // sender address
+      to: `${newUser.email}`, // list of receivers
+      subject: "Welcome to Treasure Hunt 🏴‍☠️", // Subject line
+      text: `Hello ${newUser.username}, 
+      Welcome to Treasure Hunt Website.
+      
+      Regards,
+      Treasure Hunt Team ❤`, // plain text body
+    });
+    console.log("email ::::::", info);
+  } catch (error) {
+    console.log("Sending Email Error::", error);
+  }
+};
 
 exports.signup = async (req, res, next) => {
   const { password } = req.body;
@@ -16,6 +48,7 @@ exports.signup = async (req, res, next) => {
       exp: Date.now() + JWT_EXPIRATION_MS,
     };
     const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
+    sendEmail(newUser);
     res.status(201).json({ token });
   } catch (error) {
     next(error);
